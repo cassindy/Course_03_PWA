4eddb6bc89c1a759d0024c4f288f1f0b61645e96
cassindy <cassindy.tw@gmail.com>
2025-08-24 19:47:37 +0800

    be4 service worker

--- js/app.js
+++ js/app.js
@@ -1,20 +1,30 @@
 const newPeriodFormEl = document.getElementsByTagName("form")[0];
-const startDateInputEl = document.getElementById("start-date");
-const endDateInputEl = document.getElementById("end-date");
+const startDInputEl = document.getElementById("start-date");
+const endDInputEl = document.getElementById("end-date");
+const pastPeriodsUl = document.getElementById("pastPeriodsUl");
+const btnSubmit = document.getElementById("btnSubmit");
+const btnTest = document.getElementById("btnTest");
 
 const STORAGE_KEY = "period-tracker";
 
-newPeriodFormEl.addEventListener("submit",(event)=>{
-    event.preventDefault();
-    const startDate = startDateInputEl.value;
-    const endDate = endDateInputEl.value;
+renderPastPeriods();
 
-    if (checkDatesInvalid(startDate,endDate)){
+btnTest.addEventListener("click",(event)=>{
+    window.localStorage.clear(STORAGE_KEY);
+    renderPastPeriods()
+});
+
+btnSubmit.addEventListener("click",(event)=>{
+    event.preventDefault();
+    const startD = startDInputEl.value;
+    const endD = endDInputEl.value;
+
+    if (checkDatesInvalid(startD,endD)){
         newPeriodFormEl.reset();
         return
     }
 
-    storeNewPeriod(startDate,endDate);
+    storeNewPeriod(startD,endD);
 
     renderPastPeriods();
 
@@ -31,16 +41,67 @@ function checkDatesInvalid(start,end){
 function storeNewPeriod(start,end){
     const data = getAllStortedPeriod();
     
+    // console.log("data : ",data);
+    const startD = new Date(start).toISOString().split("T")[0];
+    const endD = new Date(end).toISOString().split("T")[0];
     
+    data.push({startD,endD});
+    let sortedData = data.sort((a,b)=>{
+        return new Date(b.startD) - new Date(a.startD)
+    });
+
+    // console.log(data);
+    window.localStorage.setItem(STORAGE_KEY,JSON.stringify(sortedData));
 }
 
 function getAllStortedPeriod(){
     const data = window.localStorage.getItem(STORAGE_KEY);
-    const period = data ? JSON.parse(data) : [];
+    // console.log(data);
+    // let period = data ? JSON.parse(data) : [];
+    let periods;
+    try{
+        periods = data ? JSON.parse(data) : [];
+    }catch{
+        periods = []; // 解析失败时也返回空数组
+    }
 
-    return period;
+    return periods;
 }
 
 function renderPastPeriods(){
+    pastPeriodsUl.innerHTML = "";
+    const data = JSON.parse(window.localStorage.getItem(STORAGE_KEY));
+    
+    // pastPeriodsUl;
+    
+    const liNode = [];
+    try{for(let i=0;i<data.length;i++){
+        const liStart = document.createElement("li");
+        const liEnd = document.createElement("li");
 
+        liStart.innerText = (i+1)+" : from tw "+formatDate(data[i].startD);
+        liEnd.innerText =   (i+1)+" :   to "+formatDate(data[i].endD);
+
+        liNode.push(liStart);
+        liNode.push(liEnd);
+    }}catch{
+        
+        
+    }
+    
+    
+    liNode.forEach(li=>pastPeriodsUl.appendChild(li));
+
+    checkSecurity();
+}
+
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("zh-TW", { timeZone: "UTC" });
+}
+
+function checkSecurity(){
+    if (window.isSecureContext) {
+        console.log("this window isSecureContext ");
+    }
 }
